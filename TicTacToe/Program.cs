7935eb2983b5fd4bd372a96ba280518523e623dd
Apoorva1217 <apoorvarasal12@gmail.com>
2020-12-06 17:24:42 +0530

    [Apoorva] Add : As a Player would like to start fresh by creating a tic tac toe board

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -2,11 +2,25 @@
 
 namespace TicTacToe
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Tic Tac Toe Problem!");
+            char[] board = CreateBoard();
+        }
+
+        /// <summary>
+        /// UC1 As a Player would like to start fresh by creating a tic tac toe board
+        /// </summary>
+        private static char[] CreateBoard()
+        {
+            char[] board = new char[10];
+            for (int index = 0; index < board.Length; index++)
+            {
+                board[index] = ' ';
+            }
+            return board;
         }
     }
 }
