77bff39184c33b46180a4f8bea88ae64f4e8a9ad
Apoorva1217 <apoorvarasal12@gmail.com>
2020-12-06 18:03:48 +0530

    [Apoorva] Add : Ability for user to make a move to a desired location in the board

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -10,6 +10,7 @@ namespace TicTacToe
             char[] board = CreateBoard();
             char userLetter = ChooseUserLetter();
             ShowBoard(board);
+            int userMove = GetUserMove(board);
         }
 
         /// <summary>
@@ -38,7 +39,7 @@ namespace TicTacToe
         /// <summary>
         /// UC3 Write a method showBoard to display the current Board
         /// </summary>
-        /// <param name="board">The board.</param>
+        /// <param name="board"></param>
         private static void ShowBoard(char[] board)
         {
             Console.WriteLine("\n " + board[1] + " | " + board[2] + " | " + board[3]);
@@ -46,6 +47,34 @@ namespace TicTacToe
             Console.WriteLine(" " + board[4] + " | " + board[5] + " | " + board[6]);
             Console.WriteLine("-----------");
             Console.WriteLine(" " + board[7] + " | " + board[8] + " | " + board[9]);
+        }
+
+        /// <summary>
+        /// UC4 Ability for user to make a move to a desired location in the board
+        /// </summary>
+        /// <param name="board"></param>
+        /// <returns></returns>
+        private static int GetUserMove(char[] board)
+        {
+            int[] validCells = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            while (true)
+            {
+                Console.WriteLine("\nWhat is your next move? (1-9) : ");
+                int index = Convert.ToInt32(Console.ReadLine());
+                if (Array.Find<int>(validCells, element => element == index) != 0 && IsSpaceFree(board, index));
+                return index;
+            }
+        }
+
+        /// <summary>
+        /// Check whether space is free
+        /// </summary>
+        /// <param name="board"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        private static bool IsSpaceFree(char[] board, int index)
+        {
+            return board[index] == ' ';
         }
     }
 }
